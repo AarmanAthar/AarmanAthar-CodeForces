a6736cc1e52926cfcb6d7cf169a94553c8870554
AarmanAthar <syedaarmanathar24329@gmail.com>
2026-03-01 15:53:44 +0530

    Mar 1

--- C-Shifted MEX.cpp
+++ C-Shifted MEX.cpp
@@ -1,76 +1,35 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cmath>
-using namespace std;
 
-int MEX(vector<int>arr){
-    int mex = 0;
-    int i = 0;
-    while (i<arr.size()){
-    if(mex == arr[i]){
-        mex++;
-        i=-1;
-    }
-    i++;
-}
-    return mex;
-}
+using namespace std;
 
 int main(){
     int t;
     cin >> t;
 
-        for(int k = 0; k < t; k++ ){
+        while(t--){
             int n;
             cin >> n;
-            int x;
-            vector<int>v;
-            
-            for(int i = 0; i < n; i++){
-                cin >> x;
-                v.push_back(x);
-            }
+
+            vector<int>v(n);
+            for (auto &x : v) cin >> x;
          
             sort(v.begin(),v.end());
             v.erase(unique(v.begin(),v.end()),v.end());
             
-            int l = 0;
-            int r= 0;
-            for (int i = 0; i < v.size()-1; i++){
-            if (v[i]+1==v[i+1]){
-                r = i;
-            }
-            else{
-                if ((r-l) < i-(r+1)){
-                    l = r + 1;
-                    r = i;
+            n = v.size();
+            int best = 0;
+            int current = 0;
+
+            for(int i=0; i<n; i++){
+                if (i == 0 || v[i] != v[i-1]+1){
+                    current = 0;
                 }
+                current++;
+                best  = max(best,current);
             }
-            cout << "l:" << l << " , r:" << r << endl;
+            cout << best << endl;
         }
         
         }
-        cout << endl;
-        }
-    
-            
-// 4          
-// 0123
-// 1
-// 2346
-// -10124
-// -112356
-            
-
-
-
-
-
-
-
-
-
-
-
-
